8bc31561c7610859b8285ce0049070c4eea7efba
Garefild <Garefild2@gmail.com>
2024-11-30 22:28:27 +0200

    index: export supportUtilInspect() and setGlobalBuffer()

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,4 @@ export type * from '@providers/interfaces/buffer.interfaces';
 export * from '@providers/buffer.provider';
 export * from '@components/utf8.component';
 export * from '@components/charset.component';
+export * from '@components/globals.component';
